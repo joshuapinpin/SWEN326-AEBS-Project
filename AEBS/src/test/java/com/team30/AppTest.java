aa129e54f95ca3558753d800ef9394edec62c5d9
pinpinjosh <pinpinjosh@ecs.vuw.ac.nz>
2026-05-05 12:04:04 +1200

    feat(test): changed testing envrionment to junit based

--- AEBS/src/test/java/com/team30/AppTest.java
+++ AEBS/src/test/java/com/team30/AppTest.java
@@ -1,38 +1,16 @@
 package com.team30;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App using JUnit 5 (Jupiter).
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
