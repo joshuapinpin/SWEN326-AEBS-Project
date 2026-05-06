0dbf78e97a0d04322cfe0c50f45fbba87f19cce9
hayley-far <hayleyfar5@gmail.com>
2026-05-06 15:48:02 +1200

    feat(sensors): add SensorType and SensorId enums

---
+++ AEBS/src/main/java/com/team30/core/datalayer/SensorId.java
@@ -0,0 +1,10 @@
+package com.team30.core.datalayer;
+
+/**
+ * Enum representing the identifiers for primary and redundant sensors in the AEBS system.
+ * @author Hayley Far
+ */
+public enum SensorId {
+    PRIMARY,
+    REDUNDANT
+}
