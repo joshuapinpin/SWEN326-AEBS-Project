6c5c7505bfe36173d65f39d4850b5365e49f453d
hayley-far <hayleyfar5@gmail.com>
2026-05-09 00:23:18 +1200

    feat(enums): implementing all the classes in the enums package

--- AEBS/src/main/java/com/team30/core/datalayer/enums/MovementDirection.java
+++ AEBS/src/main/java/com/team30/core/datalayer/enums/MovementDirection.java
@@ -1,4 +1,11 @@
 package com.team30.core.datalayer.enums;
 
+/**
+ * Enum representing the movement direction of an object relative to the ego vehicle in the AEBS system.
+ * Defines categories such as SAME_DIRECTION, CROSSING, and STATIONARY to classify the movement patterns of detected objects.
+ */
 public enum MovementDirection {
+    SAME_DIRECTION,
+    CROSSING,
+    STATIONARY
 }
--- AEBS/src/main/java/com/team30/core/datalayer/enums/SensorType.java
+++ AEBS/src/main/java/com/team30/core/datalayer/enums/SensorType.java
@@ -2,7 +2,7 @@ package com.team30.core.datalayer.enums;
 
 /**
  * Enum representing different types of sensors used in the AEBS system.
- * @author Hayley Far
+ * Defines the categories of sensors that can provide data to the system, such as RADAR, LIDAR, CAMERA, and WHEEL_SPEED.
  */
 public enum SensorType {
     RADAR,
