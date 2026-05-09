28ee853a800e4ceebc0631f81f585c88485bd92b
emilyung <ungemil@ecs.vuw.ac.nz>
2026-05-09 21:53:08 +1200

    Adding braking controller and fault handler logic

--- AEBS/src/main/java/com/team30/core/datalayer/enums/BrakeResult.java
+++ AEBS/src/main/java/com/team30/core/datalayer/enums/BrakeResult.java
@@ -1,6 +1,7 @@
 package com.team30.core.datalayer.enums;
 
 /**
+ *
  * Enum representing the possible results of a braking action in the AEBS system.
  * Defines outcomes such as SUCCESS, FAILED, EXHAUSTED, CLEARED, and NOT_NEEDED to indicate the status of brake interventions.
  */
